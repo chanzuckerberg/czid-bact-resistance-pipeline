5bcfb2ca15ccd91c5f3ad33a1eb54afc97bbbd81
Meera <msrinivasan@ip-172-31-22-217.us-west-1.compute.internal>
2018-06-20 23:07:14 +0000

    Initial version of script to download AMR files and run srst2

---
+++ align_amr.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+# arguments: project_id, sample_id, output_dir, min_coverage, n_threads
+PROJECT_ID=$1
+SAMPLE_ID=$2
+OUTPUT_DIR=$3
+MIN_COVERAGE=$4
+N_THREADS=$5
+echo $PROJECT_ID
+echo $SAMPLE_ID
+echo $OUTPUT_DIR
+echo $MIN_COVERAGE
+echo $N_THREADS
+aws s3 cp --recursive s3://idseq-samples-prod/samples/$PROJECT_ID/$SAMPLE_ID/fastqs/ $OUTPUT_DIR
+#inputFiles = ($OUTPUT_DIR/*)
+INPUT=""
+space=" "
+for file in $OUTPUT_DIR/*
+do
+	# TODO: check
+	INPUT=$INPUT$file$space
+done
+
+echo "Input command to srst2: "
+echo $INPUT
+output="output"
+
+srst2 --input_pe $INPUT --forward 001 --reverse 002 --min_coverage $MIN_COVERAGE --threads $N_THREADS --output $SAMPLE_ID$output --log --gene_db ~/miniconda3/bin/srst2/data/ARGannot_r2.fasta
